076b86bb5d4bd78d1a4614b0b81d0205d38f3031
刘启明 <aehyok@163.com>
2021-04-28 22:57:07 +0800

    feat: 下拉数据切换为rap2.taobao.org

--- src/mock/api.js
+++ src/mock/api.js
@@ -1,21 +1,8 @@
 // 引入axios
  import axios from 'axios'
 
+ let url ='http://rap2api.taobao.org/app/mock/282580'
  // 内容类型
 export const getContentTypeList = () => {
-  return axios('/get/contentType')
+  return axios(`${url}/get/contentType`)
 }
-
-// 灾情类型
-export const getPestTypeList = () => {
-  return axios('/get/pestTypeList')
-}
-
-// 灾情等级
-export const getPestLevelList = () => {
-  return axios('/get/pestLevelList')
-}
-
-export const getTableAuditStatus=()=>{
-    return axios('/get/table/auditStatus')
- }

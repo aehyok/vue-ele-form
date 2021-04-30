3d1c3bc31d8c4ea502939b38afa39d410340a34e
刘启明 <455043818@qq.com>
2021-04-30 14:43:37 +0800

    添加运行demo预览页面

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,5 @@
-# aehyok-form
-A vue component for dynamic form create.
-### 本项目适合Vue2 ElementUI下进行使用的，目前基础功能已经具备。
-
-### clone后可以直接   npm install,然后npm  run start 即可开启demo查看。
-
-# install
-```
-npm install aehyok-form -S
-```
-
-# use
-##### 用法一：
-
-```
 <template>
-  <div>
+  <div id="app">
     <el-row :gutter="20">
       <el-col :span="12">
         <el-form :model="formConfig.formData" label-width="120px" ref="refForm">
@@ -48,23 +33,19 @@ npm install aehyok-form -S
   </div>
 </template>
 <script>
-import FormView from 'aehyok-form'
-import { getContentTypeList } from '@/mock/api'
+import FormView from './components/input/formView'
 import VJsoneditor from 'v-jsoneditor'
 
 export default {
   components: {
     FormView,
-    VJsoneditor,
+    VJsoneditor
   },
-  created() {
-    getContentTypeList().then(res => {
-      console.log(res.data, 'res.data.form')
-    })
-    // this.value=
+  created () {
+    console.log(this.$router, 'this.$router', 'form-app')
   },
   methods: {
-    submitForm() {
+    submitForm () {
       this.$refs.refForm.validate(valid => {
         if (valid) {
           console.log(valid, 'this.valid')
@@ -73,9 +54,9 @@ export default {
         }
         return false
       })
-    },
+    }
   },
-  data() {
+  data () {
     return {
       options: { mode: 'code', mainMenuBar: false },
       formConfig: {
@@ -85,36 +66,56 @@ export default {
             name: 'name1',
             type: 'text',
             title: '栏目标题',
-            required: true, // 必填
+            required: true // 必填
+          },
+          {
+            name: 'name',
+            type: 'text',
+            title: '栏目名称'
           },
           {
             name: 'total',
             type: 'number',
             title: '栏目数量',
-            required: true,
+            required: true
           },
           {
             name: 'count',
             type: 'number',
-            title: '浏览数量',
+            title: '浏览数量'
           },
           {
             name: 'descript',
             type: 'textarea',
             title: '备注',
-            required: true,
+            required: true
+          },
+          {
+            name: 'content',
+            type: 'textarea',
+            title: '内容'
           },
           {
             name: 'startDate',
             type: 'date',
             title: '开始日期',
-            required: true,
+            required: true
+          },
+          {
+            name: 'endDate',
+            type: 'date',
+            title: '结束日期'
+          },
+          {
+            name: 'isValid',
+            type: 'switch',
+            title: '是否有效'
           },
           {
             name: 'isExpired',
             type: 'switch',
             title: '是否过期',
-            required: true,
+            required: true
           },
           {
             name: 'type',
@@ -128,9 +129,14 @@ export default {
                     name: 'show',
                     type: 'radio',
                     title: '测试类型',
-                    required: true,
+                    required: true
+                  },
+                  {
+                    name: 'image1',
+                    type: 'image',
+                    title: '文件'
                   }
-                ],
+                ]
               },
               {
                 value: 2,
@@ -138,39 +144,39 @@ export default {
                   {
                     name: 'isValids',
                     type: 'switch',
-                    title: '是否有效',
-                  },
-                ],
-              },
-            ],
+                    title: '是否有效'
+                  }
+                ]
+              }
+            ]
           },
           {
             name: 'requireType',
             type: 'radio',
             title: '图文类型',
-            required: true,
+            required: true
           },
           {
             name: 'range',
             type: 'checkbox',
             title: '发布范围',
-            required: true,
+            required: true
           },
           {
             name: 'dateRange',
             type: 'daterange',
-            title: '日期范围',
+            title: '日期范围'
           },
           {
             name: 'creType',
             type: 'select',
-            title: '证件类型',
+            title: '证件类型'
           },
           {
             name: 'image',
             type: 'image',
-            title: '头像',
-          },
+            title: '头像'
+          }
         ],
         formData: {
           name: '主菜单栏目',
@@ -182,20 +188,11 @@ export default {
           creType: '',
           range: [],
           isExpired: false,
-          isValid: true,
-        },
-      },
+          isValid: true
+        }
+      }
     }
-  },
+  }
 }
 </script>
 <style scoped></style>
-
-```
-
-##### 用法二
-直接clone本仓库，然后将对应文件夹的代码（src/components/input）拷贝到自己的项目中
-  ```javascript
-    import FormView from './components/input/formView'
-    剩余用法跟用法一一致
-  ```
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
-const path = require('path')
+// const path = require('path')
 
-module.exports = {
-  // publicPath: '/dist/',
-  css: {
-    extract: false
-  },
-  // // outputDir: path.resolve(__dirname, './dist'),
-  // configureWebpack: {
-  //   output: {
-  //     filename: 'aehyok-form.min.js',
-  //     library: 'aehyok-form',
-  //     libraryTarget: 'umd',
-  //     umdNamedDefine: true
-  //   }
-  // },
-  // productionSourceMap: false
-}
+// module.exports = {
+//   // publicPath: '/dist/',
+//   css: {
+//     extract: false
+//   },
+//   // // outputDir: path.resolve(__dirname, './dist'),
+//   // configureWebpack: {
+//   //   output: {
+//   //     filename: 'aehyok-form.min.js',
+//   //     library: 'aehyok-form',
+//   //     libraryTarget: 'umd',
+//   //     umdNamedDefine: true
+//   //   }
+//   // },
+//   // productionSourceMap: false
+// }
